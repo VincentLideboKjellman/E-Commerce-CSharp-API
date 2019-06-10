9856d8e82268de515b1bc49cf07048a975f90659
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-06-10 21:38:35 +0200

    Added cart repository and service

---
+++ E-Commerce/Repositories/CartRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using Dapper;
+using E_Commerce.Models;
+
+namespace E_Commerce.Repositories
+{
+    public class CartRepository
+    {
+        private string connectionString;
+
+        public CartRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public List<Cart> Get(string guid)
+        {
+            using (var connection = new SQLiteConnection(this.connectionString))
+            {
+                //Make it work with the database
+                //return connection.Query<Cart>("SELECT cart_items.id, cart_guid, product_id, product_qty, product_name, product_img, product_description from cart_items LEFT JOIN products ON cart_items.product_id = products.id WHERE cart_guid =  @guid", new { guid }).ToList();
+            }
+        }
+    }
+}
---
+++ E-Commerce/Services/CartService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using E_Commerce.Repositories;
+using E_Commerce.Models;
+
+namespace E_Commerce.Services
+{
+    public class CartService
+    {
+        //Dont forget to arr repositories
+        private CartRepository cartRepository;
+
+        public CartService(CartRepository cartRepository)
+        {
+            this.cartRepository = cartRepository;
+        }
+
+
+        public List<Cart> Get(string guid)
+        {
+            return this.cartRepository.Get(guid);
+        }
+    }
+}
